d4dc23cfa5e83ccb4493f72945636cb8cb4f5089
Tomasz Rudowicz <tomasz@rudowi.cz>
2016-12-09 10:17:34 +0100

    added files

---
+++ js/hanger_man-eng.js
@@ -0,0 +1,272 @@
+	var x = Math.floor((Math.random() * 120) + 0);
+	var tab_pass = new Array(120);
+
+//  PRZYSZŁOWIA
+	tab_pass[0] = "ALL THE GLITTERS IS NOT GOLD";
+	tab_pass[1] = "BAD NEWS TRAVELS FAST";
+	tab_pass[2] = "BETTER LATE THAN NEVER";
+	tab_pass[3] = "BETTER SAFE THAN SORRY";
+	tab_pass[4] = "CHARITY BEGINS AT HOME";
+	tab_pass[5] = "DONT CRY OVER SPILLED MILK";
+	tab_pass[6] = "FOREWARNED IS FOREARMED";
+	tab_pass[7] = "GOOD THINGS COME IN SMALL PACKAGES";
+	tab_pass[8] = "HASTE MAKE WASTE";
+	tab_pass[9] = "HE WHO HESITATES IS LOST";
+	tab_pass[10] = "HE WHO LAUGHS LAST LAUGHS BEST";
+	tab_pass[11] = "IT TAKES TWO TO TANGO";
+	tab_pass[12] = "IT NEVER RAINS BUT IT POURS";
+	tab_pass[13] = "IF YOU CANT BEAT THEM JOIN THEM";
+	tab_pass[14] = "LEAVE WELL ENOUGH ALONE";
+	tab_pass[15] = "LOVE IS BLIND";
+	tab_pass[16] = "LOOK BEFORE YOU LEAP";
+	tab_pass[17] = "MIGHT MAKES RIGHT";
+	tab_pass[18] = "MONEY DOES NOT GROW ON TREES";
+	tab_pass[19] = "NO PAIN NO GAIN";
+	tab_pass[20] = "NOTHING HURTS LIKE THE TRUTH";
+	tab_pass[21] = "NO NEWS IS GOOD NEWS";
+	tab_pass[22] = "OLD HABITS DIE HARD";
+	tab_pass[23] = "PRACTICE MAKES PERFECT";
+	tab_pass[24] = "ROME WASNT BUILT IN A DAY";
+	tab_pass[25] = "STRIKE WHILE THE IRON IS HOT";
+	tab_pass[26] = "THERE IS NO HONOR AMONG THIEVES";
+	tab_pass[27] = "THERE IS NO PLACE LIKE HOME";
+	tab_pass[28] = "TWO HEADS ARE BETTER THAN ONE";
+	tab_pass[29] = "VARIETY IS THE SPICE OF LIFE";
+	tab_pass[30] = "YOU REAP WHAT YOU SOW";
+	tab_pass[31] = "YOU ARE NEVER TOO OLD TO LEARN";
+	tab_pass[32] = "MAN CAN DIE JUST ONCE";
+	tab_pass[33] = "POOR MAN IS ALWAYS BEHIND";
+	tab_pass[34] = "DONT JUDGE A BOOK BY COVER";
+	tab_pass[35] = "EASY COME EASY GO";
+	tab_pass[36] = "GOLD IS WHERE YOU FIND IT";
+	tab_pass[37] = "FISH OR CUT BAIT";
+	tab_pass[38] = "IGNORANCE IS BLISS";
+	tab_pass[39] = "LET THE BIG DOG EAT";
+
+//  FILMY
+
+	tab_pass[40] = "TITANIC";
+	tab_pass[41] = "SHAWSHANK REDEMPTION";
+	tab_pass[42] = "INTOUCHABLES";
+	tab_pass[43] = "THE GODFATHER";
+	tab_pass[44] = "THE GREEN MILE";
+	tab_pass[45] = "TWELVE ANGRY MEN";
+	tab_pass[46] = "PULP FICTION";
+	tab_pass[47] = "SCHINDLER LIST";
+	tab_pass[48] = "LORD OF THE RINGS";
+	tab_pass[49] = "FIGHT CLUB";
+	tab_pass[50] = "FORREST GUMP";
+	tab_pass[51] = "GOODFELLAS";
+	tab_pass[52] = "DJANGO UNCHAINED";
+	tab_pass[53] = "BEAUTIFUL MIND";
+	tab_pass[54] = "SILENCE OF THE LAMBS";
+	tab_pass[55] = "INCEPTION";
+	tab_pass[56] = "THE PIANIST";
+	tab_pass[57] = "SCARFACE";
+	tab_pass[58] = "THE LION KING";
+	tab_pass[59] = "GRAN TORINO";
+	tab_pass[60] = "SHUTTER ISLAND";
+	tab_pass[61] = "BREAKING BAD";
+	tab_pass[62] = "BRAVEHEART";
+	tab_pass[63] = "INDIANA JONES";
+	tab_pass[64] = "THE PRESTIGE";
+	tab_pass[65] = "HOUSE OF CARDS";
+	tab_pass[66] = "SUITS";
+	tab_pass[67] = "DEXTER";
+	tab_pass[68] = "DR HOUSE";
+	tab_pass[69] = "LOST";
+	tab_pass[70] = "APOCALYPSE NOW";
+	tab_pass[71] = "JAMES BOND";
+	tab_pass[72] = "INTERSTELLAR";
+	tab_pass[73] = "SEVEN POUNDS";
+	tab_pass[74] = "SNATCH";
+	tab_pass[75] = "STAR WARS";
+	tab_pass[76] = "GAME OF THRONES";
+	tab_pass[77] = "TRUE DETECTIVE";
+	tab_pass[78] = "MONTY PYTHON";
+	tab_pass[79] = "TWIN PEAKS";
+	tab_pass[80] = "MR ROBOT";
+
+//ZESPOŁY
+	tab_pass[81] = "BON JOVI";
+	tab_pass[82] = "MUSE";
+	tab_pass[83] = "PEARL JAM";
+	tab_pass[84] = "OASIS";
+	tab_pass[85] = "FOO FIGHTERS";
+	tab_pass[86] = "RAMONES";
+	tab_pass[87] = "RADIOHEAD";
+	tab_pass[88] = "ABBA";
+	tab_pass[89] = "RED HOT CHILLI PEPPERS";
+	tab_pass[90] = "LINKIN PARK";
+	tab_pass[91] = "GREEN DAY";
+	tab_pass[92] = "AEROSMITH";
+	tab_pass[93] = "THE WHO";
+	tab_pass[94] = "DIRE STRAITS";
+	tab_pass[95] = "LED ZEPPELIN";
+	tab_pass[96] = "ARCHIVE";
+	tab_pass[97] = "QUEEN";
+	tab_pass[98] = "THE DOORS";
+	tab_pass[99] = "PINK FLOYD";
+	tab_pass[100] = "JOHN LENNON";
+	tab_pass[101] = "METALLICA";
+	tab_pass[102] = "BLACK SABBATH";
+	tab_pass[103] = "NIRVANA";
+	tab_pass[104] = "GUNS N ROSES";
+	tab_pass[105] = "THE BEATLES";
+	tab_pass[106] = "AC DC";
+	tab_pass[107] = "DEEP PURPLE";
+	tab_pass[108] = "DEPECHE MODE";
+	tab_pass[109] = "LOUIS ARMSTRONG";
+	tab_pass[110] = "JIMI HENDRIX";
+	tab_pass[111] = "BOB DYLAN";
+	tab_pass[112] = "BRUCE SPRINGSTEEN";
+	tab_pass[113] = "SINEAD OCONNOR";
+	tab_pass[114] = "ADELE";
+	tab_pass[115] = "ERIC CLAPTON";
+	tab_pass[116] = "THE ROLLING STONES";
+	tab_pass[117] = "IRON MAIDEN";
+	tab_pass[118] = "MICHAEL JACKSON";
+	tab_pass[119] = "BOB MARLEY";
+	tab_pass[120] = "THE POLICE";
+	tab_pass[121] = "STING";
+
+// określenie kategorii
+
+
+//przysłowia
+if (x>=0&&x<40)
+{
+var category = "PROVERBS";
+}
+//filmy
+else if (x>=40&&x<81)
+{
+var category = "MOVIES AND SERIES";
+}
+//muzyka
+else if (x>=81&&x<122)
+{
+var category = "MUSIC";
+}
+
+		var pass = tab_pass[x];
+
+
+var length = pass.length;
+var pass1 = "";
+var num_of_fail = 0;
+
+
+for(i=0; i<length; i++)
+{
+	if(pass.charAt(i)==" ") pass1 = pass1+" ";
+	else pass1=pass1+"-";
+}
+
+
+function write_pass()
+{
+	document.getElementById("text-container").innerHTML = pass1;
+}
+
+window.onload = start;
+
+var tab = new Array(35);
+tab[0] = "A";
+tab[1] = "B";
+tab[2] = "C";
+tab[3] = "D";
+tab[4] = "E";
+tab[5] = "F";
+tab[6] = "G";
+tab[7] = "H";
+tab[8] = "I";
+tab[9] = "J";
+tab[10] = "K";
+tab[11] = "L";
+tab[12] = "M";
+tab[13] = "N";
+tab[14] = "O";
+tab[15] = "P";
+tab[16] = "Q";
+tab[17] = "R";
+tab[18] = "S";
+tab[19] = "T";
+tab[20] = "U";
+tab[21] = "V";
+tab[22] = "W";
+tab[23] = "X";
+tab[24] = "Y";
+tab[25] = "Z";
+
+function start() 
+{
+	var inside_div = "";
+
+	for (i=0;i<=25;i++)
+	{
+		var thing = "letter"+i;
+			inside_div = inside_div + '<div class="letter" onclick="check('+i+')" id="'+thing+'">'+tab[i]+'</div>'
+	}
+
+
+	document.getElementById("alphabet").innerHTML=inside_div;
+	document.getElementById("category").innerHTML = "Your random category is "+'<span style="color: yellow;">'+category+"</span>"+"!";
+
+
+	write_pass();
+}
+
+String.prototype.replaceThis = function(place,element)
+{
+	if(place>this.length-1) return this.toString();
+	else return this.substr(0,place)+element+this.substr(place+1);
+}
+
+function check(numer)
+{
+	var hit = false;
+	for(i=0;i<length;i++)
+	{
+		if(pass.charAt(i) == tab[numer])
+		{
+			pass1 = pass1.replaceThis(i,tab[numer]);
+			hit=true;
+		}
+	}
+
+	if(hit==true)
+	{
+		var element = "letter"+numer;
+		document.getElementById(element).style.background = "#003300";
+		document.getElementById(element).style.color = "#00C000";
+		document.getElementById(element).style.border = "3px solid #00B01A";
+		document.getElementById(element).style.cursor = "default";
+
+		write_pass();
+	}
+	else
+	{
+		//zly wybor
+		num_of_fail++;
+		var element = "letter"+numer;
+		document.getElementById(element).style.background = "#330000";
+		document.getElementById(element).style.color = "#C00000";
+		document.getElementById(element).style.border = "3px solid #C00000";
+		document.getElementById(element).style.cursor = "default";
+		document.getElementById(element).setAttribute("onclick",";");
+		document.getElementById("hanging-man").innerHTML = '<img src="img/s'+num_of_fail+'.jpg" alt="" />';
+	}
+
+	//wygrana
+	if(pass==pass1)
+	{
+		document.getElementById("alphabet").innerHTML = 'You have saved Leonardo, congratulation!<br/> You have entered the correct password, which is:<br/><span style="font-size:32px; color:green;">'+pass+'</span><br/><br/><span class="reset" onclick="location.reload()" style="cursor:pointer;">ONE MORE GAME?</span>'
+	}
+
+	//przegrana
+	if(num_of_fail >= 9)
+	{
+	document.getElementById("alphabet").innerHTML = 'You have failed!<br/><b>Leonardo Dicaprio is now sad because of you!</b><br/>The correct password is:<br/><span style="font-size:32px; color:red;">'+pass+'</span><br/><br/><span class="reset" onclick="location.reload()">ONE MORE GAME?</span>'
+	}
+}
---
+++ js/hanger_man.js
@@ -0,0 +1,282 @@
+	var x = Math.floor((Math.random() * 120) + 0);
+	var tab_pass = new Array(120);
+
+//  PRZYSZŁOWIA
+	tab_pass[0] = "CZAS TO PIENIĄDZ";
+	tab_pass[1] = "CO NAGLE TO PO DIABLE";
+	tab_pass[2] = "DOBRA MINA DO ZŁEJ GRY";
+	tab_pass[3] = "SZCZĘŚLIWI CZASU NIE LICZĄ";
+	tab_pass[4] = "BEZ PRACY NIE MA KOŁACZY";
+	tab_pass[5] = "KULA U NOGI";
+	tab_pass[6] = "PRACOWITY JAK PSZCZOŁA";
+	tab_pass[7] = "CEL UŚWIĘCA ŚRODKI";
+	tab_pass[8] = "CO DWIE GŁOWY TO NIE JEDNA";
+	tab_pass[9] = "CO MA PIERNIK DO WIATRAKA";
+	tab_pass[10] = "DO WESELA SIĘ ZAGOI";
+	tab_pass[11] = "CO ZA DUŻO TO NIE ZDROWO";
+	tab_pass[12] = "GŁUPI MA SZCZĘŚCIE";
+	tab_pass[13] = "GOŚĆ W DOM BÓG W DOM";
+	tab_pass[14] = "JAKA PRACA TAKA PŁACA";
+	tab_pass[15] = "JAK CIĘ WIDZĄ TAK CIĘ PISZĄ";
+	tab_pass[16] = "JAK KUBA BOGU TAK BÓG KUBIE";
+	tab_pass[17] = "JAK TRWOGA TO DO BOGA";
+	tab_pass[18] = "JEDNA JASKÓŁKA WIOSNY NIE CZYNI";
+	tab_pass[19] = "KLĄĆ JAK SZEWC";
+	tab_pass[20] = "KŁAMSTWO MA KRÓTKIE NOGI";
+	tab_pass[21] = "KOMU W DROGĘ TEMU CZAS";
+	tab_pass[22] = "KTO PIERWSZY TEN LEPSZY";
+	tab_pass[23] = "KTO PYTA NIE BŁĄDZI";
+	tab_pass[24] = "KTO SIĘ CZUBI TEN SIĘ LUBI";
+	tab_pass[25] = "LEJE JAK Z CEBRA";
+	tab_pass[26] = "LICHO NIE ŚPI";
+	tab_pass[27] = "ŁATWO PRZYSZŁO ŁATWO POSZŁO";
+	tab_pass[28] = "MASZ BABO PLACEK";
+	tab_pass[29] = "MĄDRY POLAK PO SZKODZIE";
+	tab_pass[30] = "NADZIEJA MATKĄ GŁUPICH";
+	tab_pass[31] = "W GORĄCEJ WODZIE KĄPANY";
+	tab_pass[32] = "PŁAKAĆ NAD ROZLANYM MLEKIEM";
+	tab_pass[33] = "WYWOŁYWAĆ WILKA Z LASU";
+	tab_pass[34] = "PIERWSZE KOTY ZA PŁOTY";
+	tab_pass[35] = "RAZ NA WOZIE RAZ POD WOZEM";
+	tab_pass[36] = "STARA MIŁOŚĆ NIE RDZEWIEJE";
+	tab_pass[37] = "STRZEŻONEGO PAN BÓG STRZEŻE";
+	tab_pass[38] = "ZŁEGO DIABLI NIE BIORĄ";
+	tab_pass[39] = "WYSZŁO SZYDŁO Z WORKA";
+
+//  FILMY
+
+	tab_pass[40] = "TITANIC";
+	tab_pass[41] = "ŚWIAT WEDŁUG KIEPSKICH";
+	tab_pass[42] = "KUBUŚ PUCHATEK";
+	tab_pass[43] = "NIETYKALNI";
+	tab_pass[44] = "OJCIEC CHRZESTNY";
+	tab_pass[45] = "REQUIEM DLA SNU";
+	tab_pass[46] = "ZIELONA MILA";
+	tab_pass[47] = "BOLEK I LOLEK";
+	tab_pass[48] = "LEON ZAWODOWIEC";
+	tab_pass[49] = "SZÓSTY ZMYSŁ";
+	tab_pass[50] = "FORREST GUMP";
+	tab_pass[51] = "PULP FICTION";
+	tab_pass[52] = "INCEPCJA";
+	tab_pass[53] = "PODZIEMNY KRĄG";
+	tab_pass[54] = "PIANISTA";
+	tab_pass[55] = "DJANGO";
+	tab_pass[56] = "PIĘKNY UMYSŁ";
+	tab_pass[57] = "KRÓL LEW";
+	tab_pass[58] = "KEVIN SAM W DOMU";
+	tab_pass[59] = "BOGOWIE";
+	tab_pass[60] = "GRA O TRON";
+	tab_pass[61] = "BREAKING BAD";
+	tab_pass[62] = "EFEKT MOTYLA";
+	tab_pass[63] = "INDIANA JONES";
+	tab_pass[64] = "WŁADCA PIERŚCIENI";
+	tab_pass[65] = "HOUSE OF CARDS";
+	tab_pass[66] = "W GARNITURACH";
+	tab_pass[67] = "DEXTER";
+	tab_pass[68] = "DR HOUSE";
+	tab_pass[69] = "ZAGUBIENI";
+	tab_pass[70] = "HELIKOPTER W OGNIU";
+	tab_pass[71] = "SZÓSTY ZMYSŁ";
+	tab_pass[72] = "INTERSTELLAR";
+	tab_pass[73] = "ILUZJONISTA";
+	tab_pass[74] = "SAMI SWOI";
+	tab_pass[75] = "DZIEŃ ŚWIRA";
+	tab_pass[76] = "KILER";
+	tab_pass[77] = "CHŁOPAKI NIE PŁACZĄ";
+	tab_pass[78] = "SALA SAMOBÓJCÓW";
+	tab_pass[79] = "SKAZANY NA BLUESA";
+	tab_pass[80] = "PSY";
+
+//ZESPOŁY
+	tab_pass[81] = "PERFECT";
+	tab_pass[82] = "REPUBLIKA";
+	tab_pass[83] = "KULT";
+	tab_pass[84] = "DŻEM";
+	tab_pass[85] = "BUDKA SUFLERA";
+	tab_pass[86] = "LAO CHE";
+	tab_pass[87] = "CZESŁAW NIEMEN";
+	tab_pass[88] = "MAANAM";
+	tab_pass[89] = "MAREK GRECHUTA";
+	tab_pass[90] = "COMA";
+	tab_pass[91] = "GRZEGORZ TURNAU";
+	tab_pass[92] = "PIDŻAMA PORNO";
+	tab_pass[93] = "MYSLOVITZ";
+	tab_pass[94] = "DIRE STRAITS";
+	tab_pass[95] = "LED ZEPPELIN";
+	tab_pass[96] = "ARCHIVE";
+	tab_pass[97] = "QUEEN";
+	tab_pass[98] = "THE DOORS";
+	tab_pass[99] = "PINK FLOYD";
+	tab_pass[100] = "JOHN LENNON";
+	tab_pass[101] = "METALLICA";
+	tab_pass[102] = "BLACK SABBATH";
+	tab_pass[103] = "NIRVANA";
+	tab_pass[104] = "GUNS N ROSES";
+	tab_pass[105] = "THE BEATLES";
+	tab_pass[106] = "AC DC";
+	tab_pass[107] = "DEEP PURPLE";
+	tab_pass[108] = "DEPECHE MODE";
+	tab_pass[109] = "LOUIS ARMSTRONG";
+	tab_pass[110] = "JIMI HENDRIX";
+	tab_pass[111] = "BOB DYLAN";
+	tab_pass[112] = "BRUCE SPRINGSTEEN";
+	tab_pass[113] = "SINEAD OCONNOR";
+	tab_pass[114] = "ADELE";
+	tab_pass[115] = "ERIC CLAPTON";
+	tab_pass[116] = "THE ROLLING STONES";
+	tab_pass[117] = "IRON MAIDEN";
+	tab_pass[118] = "MICHAEL JACKSON";
+	tab_pass[119] = "BOB MARLEY";
+	tab_pass[120] = "THE POLICE";
+	tab_pass[121] = "STING";
+
+// określenie kategorii
+
+
+//przysłowia
+if (x>=0&&x<40)
+{
+var category = "PRZYSŁOWIA";
+}
+//filmy
+else if (x>=40&&x<81)
+{
+var category = "KINEMATOGRAFIA";
+}
+//muzyka
+else
+{
+var category = "MUZYKA";
+}
+
+
+
+	var pass = tab_pass[x];
+
+var length = pass.length;
+var pass1 = "";
+var num_of_fail = 0;
+
+
+for(i=0; i<length; i++)
+{
+	if(pass.charAt(i)==" ") pass1 = pass1+" ";
+	else pass1=pass1+"-";
+}
+
+
+function write_pass()
+{
+	document.getElementById("text-container").innerHTML = pass1;
+}
+
+window.onload = start;
+
+var tab = new Array(35);
+tab[0] = "A";
+tab[1] = "Ą";
+tab[2] = "B";
+tab[3] = "C";
+tab[4] = "Ć";
+tab[5] = "D";
+tab[6] = "E";
+tab[7] = "Ę";
+tab[8] = "F";
+tab[9] = "G";
+tab[10] = "H";
+tab[11] = "I";
+tab[12] = "J";
+tab[13] = "K";
+tab[14] = "L";
+tab[15] = "Ł";
+tab[16] = "M";
+tab[17] = "N";
+tab[18] = "Ń";
+tab[19] = "O";
+tab[20] = "Ó";
+tab[21] = "P";
+tab[22] = "Q";
+tab[23] = "R";
+tab[24] = "S";
+tab[25] = "Ś";
+tab[26] = "T";
+tab[27] = "U";
+tab[28] = "V";
+tab[29] = "W";
+tab[30] = "X";
+tab[31] = "Y";
+tab[32] = "Z";
+tab[33] = "Ż";
+tab[34] = "Ź";
+
+function start() 
+{
+	var inside_div = "";
+
+	for (i=0;i<=34;i++)
+	{
+		var thing = "letter"+i;
+			inside_div = inside_div + '<div class="letter" onclick="check('+i+')" id="'+thing+'">'+tab[i]+'</div>'
+	}
+
+
+	document.getElementById("alphabet").innerHTML=inside_div;
+	document.getElementById("category").innerHTML = "Wylosowałeś kategorię "+'<span style="color: yellow;">'+category+"</span>"+"!";
+
+
+	write_pass();
+}
+
+String.prototype.replaceThis = function(place,element)
+{
+	if(place>this.length-1) return this.toString();
+	else return this.substr(0,place)+element+this.substr(place+1);
+}
+
+function check(numer)
+{
+	var hit = false;
+	for(i=0;i<length;i++)
+	{
+		if(pass.charAt(i) == tab[numer])
+		{
+			pass1 = pass1.replaceThis(i,tab[numer]);
+			hit=true;
+		}
+	}
+
+	if(hit==true)
+	{
+		var element = "letter"+numer;
+		document.getElementById(element).style.background = "#003300";
+		document.getElementById(element).style.color = "#00C000";
+		document.getElementById(element).style.border = "3px solid #00B01A";
+		document.getElementById(element).style.cursor = "default";
+
+		write_pass();
+	}
+	else
+	{
+		//zly wybor
+		num_of_fail++;
+		var element = "letter"+numer;
+		document.getElementById(element).style.background = "#330000";
+		document.getElementById(element).style.color = "#C00000";
+		document.getElementById(element).style.border = "3px solid #C00000";
+		document.getElementById(element).style.cursor = "default";
+		document.getElementById(element).setAttribute("onclick",";");
+		document.getElementById("hanging-man").innerHTML = '<img src="img/s'+num_of_fail+'.jpg" alt="" />';
+	}
+
+	//wygrana
+	if(pass==pass1)
+	{
+		document.getElementById("alphabet").innerHTML = 'Udało Ci się uratować Leonarda, brawo!<br/> Podałeś prawidłowe hasło:<br/><span style="font-size:32px; color:green;">'+pass+'</span><br/><br/><span class="reset" onclick="location.reload()" style="cursor:pointer;">JESZCZE RAZ?</span>'
+	}
+
+	//przegrana
+	if(num_of_fail >= 9)
+	{
+	document.getElementById("alphabet").innerHTML = 'PRZEGRAŁEŚ!<br/><b>Przez Ciebie Leonardo DiCaprio jest smutny.</b><br/> Prawidłowym hasłem było:<br/><span style="font-size:32px; color:red;">'+pass+'</span><br/><br/><span class="reset" onclick="location.reload()">JESZCZE RAZ?</span>'
+	}
+}
